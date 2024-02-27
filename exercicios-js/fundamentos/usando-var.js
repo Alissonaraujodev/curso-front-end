3beef17cdb481853048ad43ba45e7c1166db1c0d
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-02-27 16:11:31 -0300

    diferença de var e let

--- exercicios-js/fundamentos/usando-var.js
+++ exercicios-js/fundamentos/usando-var.js
@@ -8,7 +8,7 @@
 }
 console.log(sera)
 
-//uma variavel(var) pode ser acessada em qualquer logar mesmo estando dentro de um bloco, ela só não pode ser acessada se estiver dentro de uma função. 
+//uma variavel(var) pode ser acessada em qualquer logar mesmo estando dentro de um bloco, ela só não pode ser acessada se estiver dentro de uma função, var age de fotma global 
 
 function teste(){
     var local = 123
