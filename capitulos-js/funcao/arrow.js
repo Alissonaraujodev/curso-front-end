3c00b699e115aad84541807ba15bbc79006ca1e1
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-03-01 17:44:12 -0300

    funções

---
+++ capitulos-js/funcao/arrow.js
@@ -0,0 +1,19 @@
+let dobro = function (a){
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a 
+
+console.log(dobro(2))
+
+let ola = function(){
+    return 'OLÁ'
+}
+
+ola = () => 'OLÁ'
+
+console.log(ola())
